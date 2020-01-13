795076d4f9d1e1ff984a8ebdb3324b32ccaef7c0
Vishnu Gunapathi <38277626+Twinji@users.noreply.github.com>
2020-01-13 12:38:26 +1030

    Update script.js

--- script.js
+++ script.js
@@ -26,4 +26,26 @@ function init(c) {}
 
 function update() {}
 
-function render(c) {}
+function render(c) {
+
+    var radius = (HEIGHT > WIDTH ? WIDTH : HEIGHT) / 2 * 0.9;
+
+    c.strokeStyle = "white";
+    c.lineWidth = 5;
+
+    c.beginPath();
+    c.arc(WIDTH / 2, HEIGHT / 2, radius, 0, 2 * Math.PI);
+    c.stroke();
+    c.closePath();
+
+}
+
+// returns the current time as a JSON object
+function date() {
+    var date = new Date();
+    return {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+    }
+}
